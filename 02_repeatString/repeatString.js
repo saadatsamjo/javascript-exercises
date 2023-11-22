acfcf229805e6bd4ad1e5e2edeb760c4bb0ee7df
Saadat <samjosaadat@eyahoo.com>
2023-11-22 21:24:45 +0000

    7 tests run

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, number) {
+    if(number<0){
+        return "ERROR"
+    } else {
+        let emptyString = "";
+        for (let i = 0; i < number; i++) {
+            emptyString = emptyString+phrase;
+        }
+        return emptyString;
+    }
 };
 
 // Do not edit below this line
